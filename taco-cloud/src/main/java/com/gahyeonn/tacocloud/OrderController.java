f4c5b260bf4b06a7395c5e356f05e3c37e8f4e46
GaHyeon <parka0017@gmail.com>
2023-12-19 00:37:52 +0900

    ch2-2. 타코 주문 폼, 주문 제출 기능 구현

---
+++ taco-cloud/src/main/java/com/gahyeonn/tacocloud/OrderController.java
@@ -0,0 +1,31 @@
+package com.gahyeonn.tacocloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j //컴파일 시에 SLF4J Logger 객체 생성
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    //타코 주문 폼을 나타냄
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+
+        return "orderForm";
+    }
+
+    //타코 주문 제출 처리
+    @PostMapping
+    public String processOrder(Order order) {
+        //Order 객체가 제출된 폼 필드와 바인딩 됨
+        log.info("Order submitted: " + order);
+
+        return "redirect:/";
+    }
+}
