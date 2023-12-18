650ba0a00a0ee064797cfa2d2ad9cd524f1bd1b1
GaHyeon <parka0017@gmail.com>
2023-12-18 16:23:08 +0900

    ch1. 메인 페이지 HTML 요청 처리 구현 & 테스트

---
+++ taco-cloud/src/main/java/com/gahyeonn/tacocloud/HomeController.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //지정된 클래스가 컴포넌트로 식별되게 하는 것이 주 목적
+public class HomeController {
+
+    @GetMapping("/") //루트 경로인 /의 웹 요청을 처리한다.
+    public String home() {
+        return "home"; //뷰 이름을 반환한다.
+    }
+}
