6844ee05fdafd752608dbde2035f5fdd9851e1b2
GaHyeon <parka0017@gmail.com>
2023-12-19 16:20:58 +0900

    ch3-1. Converter를 이용한 식자재 데이터 Ingredient 객체 변환

---
+++ taco-cloud/src/main/java/com/gahyeonn/tacocloud/web/IngredientByIdConverter.java
@@ -0,0 +1,31 @@
+package com.gahyeonn.tacocloud.web;
+
+import com.gahyeonn.tacocloud.Ingredient;
+import com.gahyeonn.tacocloud.data.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/*
+    Converter에 지정한 타입 변환이 필요할 때 convert() 자동 호출
+    String 타입의 식자재 ID를 사용해서 디비에 저장된 특정 식자재 데이터를 읽은 후 Ingredient 객체로 변환하는데 사용
+
+    Converter<String, Ingredient>
+    String: 변환할 값의 타입
+    Ingredient는 변환된 값의 타입
+ */
+@Component //스프링에 의해 자동 생성 및 주입되는 빈
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepo;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepo.findById(id);
+    }
+}
